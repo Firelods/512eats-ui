28e6518cdbebc6bc000ca22f812222049461be14
Roxane Bacon <roxane.bacon@etu.univ-cotedazur.fr>
2024-11-20 20:13:00 +0100

    ✨ Update home component layout and styles with new UI elements and functionality

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+    searchQuery: string = '';
+    isOpenNow: boolean = true;
+    categories: string[] = ['Burgers', 'Pizza', 'Sushi', 'Salads'];
+    selectedCategory: string = 'Burgers';
+    isToggled: boolean = false;
 
+    selectCategory(category: string): void {
+        this.selectedCategory = category;
+    }
+
+    onToggleChange() {
+        console.log('Toggle state:', this.isToggled);
+    }
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { PanierComponent } from './components/panier/panier.component';
 import { RestaurantsModule } from '../features/restaurants/restaurants.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [HomeComponent, PanierComponent],
-    imports: [CommonModule, RestaurantsModule],
+    imports: [CommonModule, RestaurantsModule, FormsModule],
 })
 export class CoreModule {}
