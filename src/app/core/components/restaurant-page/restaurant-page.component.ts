a73d6e3d31744fc73d69d3da025825a610f4a53b
Firelods <clement.lefevre1@etu.unice.fr>
2024-11-25 16:01:26 +0100

    🐛 Mise à jour du style de la page du restaurant et des images des plats

--- src/app/core/components/restaurant-page/restaurant-page.component.ts
+++ src/app/core/components/restaurant-page/restaurant-page.component.ts
@@ -15,7 +15,7 @@ export class RestaurantPageComponent {
         foodTypeList: ['Italian'],
         averagePrice: 2,
         description: 'Delicious Italian cuisine with a modern twist.',
-        dishPictureURLListSample: ['pizza-margherita.jpg', 'pates-carbonara.jpg'],
+        dishPictureURLListSample: ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWwNz-YV88e3LFP6iisBcZT-loky1VotV4aQ&s', 'https://assets.afcdn.com/recipe/20211214/125831_w1024h1024c1cx866cy866cxt0cyt292cxb1732cyb1732.jpg'],
     }
     dishes = [
         {
@@ -23,14 +23,14 @@ export class RestaurantPageComponent {
             description: 'Classic pizza with tomato sauce, mozzarella, and basil.',
             price: '$9',
             time: '5 min',
-            image: 'pizza-margherita.jpg',
+            image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWwNz-YV88e3LFP6iisBcZT-loky1VotV4aQ&s',
         },
         {
             name: 'Spaghetti Carbonara',
             description: 'Traditional pasta with eggs, cheese, pancetta, and pepper.',
             price: '$12',
             time: '7 min',
-            image: 'pates-carbonara.jpg',
+            image: 'https://assets.afcdn.com/recipe/20211214/125831_w1024h1024c1cx866cy866cxt0cyt292cxb1732cyb1732.jpg',
         },
         // Ajouter plus de plats ici
     ];
